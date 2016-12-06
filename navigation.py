610e1c8478b650cfb1d8c44d6c1125df172c1565
Annaleah Elizabeth Ernst <haelannaleah@gmail.com>
2016-12-06 20:05:23 +0000

    Experiment with destination

--- navigation.py
+++ navigation.py
@@ -93,7 +93,7 @@ class Navigation(Motion):
             # we've more or less reached our waypoint!
             return True
 
-        elif not np.isclose(cur_orientation, desired_turn, atol=0.01):
+        elif not np.isclose(cur_orientation, desired_turn, rtol=0.05):
             self.turn(cur_orientation < desired_turn)
 
         else:
