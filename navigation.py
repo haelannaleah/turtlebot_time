607eca58dce6bbc420f1a39115367781d42c9477
Annaleah Elizabeth Ernst <haelannaleah@gmail.com>
2017-01-10 22:27:08 +0000

    Correct x and y orientation

--- navigation.py
+++ navigation.py
@@ -184,8 +184,8 @@ class Navigation(Motion):
         
         # convert the origin tag to a offset for setting the origin
         offset = (tag_pose[0][0]**2 + tag_pose[0][1]**2)**0.5
-        x = self.cur_pose[0][0] + offset * cos(tag_pose[1])
-        y = self.cur_pose[0][1] + offset * sin(tag_pose[1])
+        x = self.cur_pose[0][0] + offset * sin(tag_pose[1])
+        y = self.cur_pose[0][1] + offset * cos(tag_pose[1])
         angle = self.cur_pose[1] + tag_pose[1] - self._HALF_PI
         
         if angle > self._TWO_PI:
