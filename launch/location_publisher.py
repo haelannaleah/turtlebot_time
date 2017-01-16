53b9b9da3ade543f871b6001748448da16aa5fae
Annaleah Elizabeth Ernst <haelannaleah@gmail.com>
2017-01-16 01:35:39 +0000

    syntax fix

--- launch/location_publisher.py
+++ launch/location_publisher.py
@@ -27,21 +27,21 @@ class FramePublisher():
 
     def publish(self, frame, publisher):
         try:
-            position, orientation = listener.lookupTransform("/base_footprint", frame, rospy.Time(0))
+            position, orientation = self.listener.lookupTransform("/base_footprint", frame, rospy.Time(0))
             print frame
             print position
             print orientation
             
-            new_pose = Pose()
-            new_pose.position.x = position[0]
-            new_pose.position.y = position[1]
-            new_pose.position.z = position[2]
-            new_pose.orientation.x = orientation[0]
-            new_pose.orientation.y = orientation[1]
-            new_pose.orientation.z = orientation[2]
-            new_pose.orientation.w = orientation[3]
+            frame_pose = Pose()
+            frame_pose.position.x = position[0]
+            frame_pose.position.y = position[1]
+            frame_pose.position.z = position[2]
+            frame_pose.orientation.x = orientation[0]
+            frame_pose.orientation.y = orientation[1]
+            frame_pose.orientation.z = orientation[2]
+            frame_pose.orientation.w = orientation[3]
             
-            publisher(new_pose)
+            publisher(frame_pose)
             
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             rospy.logwarn("Unable to publish to " + str(frame))
