e998779f0fadeb682940273c5ff2eab62bbc714a
Annaleah Elizabeth Ernst <haelannaleah@gmail.com>
2017-01-16 01:03:39 +0000

    Added publisher for map and odometry data
    First step in integrating use of the map in the project. Rough draft to begin testing.

---
+++ launch/location_publisher.py
@@ -0,0 +1,30 @@
+"""
+location_publisher.py
+
+Retreive both relative and absolute position information from the tf topic.
+Frames are fed information by robot_location
+"""
+import rospy
+import tf
+
+if __name__ == '__main__':
+    rospy.init_node('frame_transformations')
+    
+    # set up the tf node
+    listener = tf.TransformListener()
+    
+    rospy.rate(100)
+    while not rospy.is_shutdown():
+        try:
+            map_trans, map_rot = listener.lookupTransform("/base_footprint", "/map", rospy.Time(0))
+            print map_trans
+            print map_rot
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            continue
+        
+        try:
+            odom_trans, odom_rot = listener.lookupTransform("/base_footprint", "/odom", rospy.Time(0))
+            print odom_trans
+            print odom_rot
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            continue
