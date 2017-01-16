4d27fd56e701b44c8462e10d20cf370d29748181
Annaleah Ernst <annaleahernst@college.harvard.edu>
2017-01-15 21:25:19 -0500

    Added catkin workspace
    Note: to use, you must source catkin_workspace/devel/setup.bash
    otherwise, the packages contained within are inaccessible

---
+++ catkin_workspace/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/turtlebot/turtlebot_time/catkin_workspace/src
+# Build directory: /home/turtlebot/turtlebot_time/catkin_workspace/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(adventurebot_frames)
---
+++ catkin_workspace/build/adventurebot_frames/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/turtlebot/turtlebot_time/catkin_workspace/src/adventurebot_frames
+# Build directory: /home/turtlebot/turtlebot_time/catkin_workspace/build/adventurebot_frames
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
