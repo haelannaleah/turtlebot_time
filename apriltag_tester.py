cf576235d3aea8b8e421bf13c45ecc5ccbc9650b
Annaleah Ernst <annaleahernst@college.harvard.edu>
2017-01-29 16:09:56 -0500

    Changed printing slightly

--- apriltag_tester.py
+++ apriltag_tester.py
@@ -25,6 +25,23 @@ class AprilTester():
         """Extract Euler angle"""
         self.tags = data.markers if data.markers else None
 
+    def printOrientation(self):
+        if self.tags is None:
+            return
+        
+        for tag in self.tags:
+            q = tag.pose.pose.orientation
+            euler_angle = tf.transformations.euler_from_quaternion([q.x,q.y,q.z,q.w])
+            self._logger.debug(euler_angle, "orient of " + str(tag.id))
+
+    def printPosition(self):
+        if self.tags is None:
+            return
+        
+        for tag in self.tags:
+            p = tag.pose.pose.position
+            self._logger.debug((p.x,p.y,p.z), "pos of " + str(tag.id))
+
     def printTag(self):
         """Print tag data with Euler Angle"""
         if self.tags is None:
@@ -32,10 +49,10 @@ class AprilTester():
 
         for tag in self.tags:
             p = tag.pose.pose.position
-            self._logger.debug((p.x,p.y,p.z), "position of " + str(tag.id))
+            self._logger.debug((p.x,p.y,p.z), " posit of " + str(tag.id))
             q = tag.pose.pose.orientation
             euler_angle = tf.transformations.euler_from_quaternion([q.x,q.y,q.z,q.w])
-            self._logger.debug(euler_angle, "orientation of " + str(tag.id))
+            self._logger.debug(euler_angle, "orient of " + str(tag.id))
 
 if __name__ == "__main__":
     AprilTester()
