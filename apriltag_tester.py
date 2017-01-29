43e8c60fb6fe46712c8f346503c35050c344bc86
Annaleah Ernst <annaleahernst@college.harvard.edu>
2017-01-29 16:02:42 -0500

    Added class for testing AprilTags

---
+++ apriltag_tester.py
@@ -0,0 +1,41 @@
+import rospy
+import tf
+
+from ar_track_alvar_msgs.msg import AlvarMarkers
+
+from logger import Logger
+
+class AprilTester():
+    def __init__(self):
+        """Set up a node for extracting roll/pitch/yaw data from AprilTag"""
+        rospy.init_node('AprilTester', anonymous = False)
+        self._logger = Logger("AprilTester")
+        
+        
+        self.tags = None
+        rospy.Subscriber('/ar_pose_marker', AlvarMarkers, self.tagCallback, queue_size=1)
+        
+        rate = rospy.Rate(100)
+    
+        while not rospy.is_shutdown():
+            self.printTag()
+            rate.sleep()
+
+    def tagCallback(self, data):
+        """Extract Euler angle"""
+        self.tags = data.markers if data.markers else None
+
+    def printTag(self):
+        """Print tag data with Euler Angle"""
+        if self.tags is None:
+            return
+
+        for tag in self.tags:
+            p = self.tag.pose.pose.position
+            self._logger.debug((p.x,p.y,p.z), "position of " + str(tag.id))
+            q = self.tag.pose.pose.orientation
+            euler_angle = tf.transformations.euler_from_quaternion([q.x,q.y,q.z,q.w])
+            self._logger.debug(euler_angle, "orientation of " + str(tag.id))
+
+if __name__ == "__main__":
+    AprilTester()
