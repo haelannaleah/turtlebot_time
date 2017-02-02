63a7ee8cb72eaa3037bbcc0e2a183ed829a23bd4
Annaleah Ernst <annaleahernst@college.harvard.edu>
2017-02-02 14:16:19 -0500

    syntax

--- apriltag_tester.py
+++ apriltag_tester.py
@@ -1,6 +1,8 @@
 import rospy
 import tf
 
+import numpy as np
+
 from ar_track_alvar_msgs.msg import AlvarMarkers
 from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped
 from math import degrees
